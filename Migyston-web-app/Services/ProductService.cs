07c59d411ba6c43da9a33b1291dae2c7b2675c86
DamjanT1988 <Damjan_t@hotmail.com>
2023-01-16 20:14:53 +0100

    1

--- Migyston-web-app/Services/ProductService.cs
+++ Migyston-web-app/Services/ProductService.cs
@@ -107,28 +107,19 @@ namespace Migyston_web_app.Services
         }
 
 
-
-        //Define your static method which will make the method become part of the class
-        //Also make it asynchronous meaning it is retrieving data from a api.
-        //Have it void since your are logging the result into the console.
-        //Which would take a integar as a argument.
         public static async void GetProducts()
         {
-            //Define your base url
             string baseURL = "https://localhost:7014/Product";
-            //Have your api call in try/catch block.
 
-            //Now we will have our using directives which would have a HttpClient 
+
             using (HttpClient client = new HttpClient())
             {
-                //Now get your rsponse from the client from get request to baseurl.
-                //Use the await keyword since the get request is asynchronous, and want it run before next asychronous operation.
+
                 using (HttpResponseMessage res = await client.GetAsync(baseURL))
                 {
-                    //Now we will retrieve content from our response, which would be HttpContent, retrieve from the response Content property.
                     using (HttpContent content = res.Content)
                     {
-                        //Retrieve the data from the content of the response, have the await keyword since it is asynchronous.
+
                         string data = await content.ReadAsStringAsync();
 
 
@@ -159,159 +150,3 @@ namespace Migyston_web_app.Services
     }
 
 
-
-
-/*
-        public static async void SaveList()
-        {
-            //serialize the list to a variable
-            //string json = JsonSerializer.Serialize(Products);
-
-            for (int i = 0; i < Products.Count; i++)
-            {
-                var product = new Product();
-                product.product_title = Products[i].product_title;
-                product.ean_number = Products[i].ean_number;
-                product.product_description = Products[i].product_description;
-                product.amount_storage = Products[i].amount_storage;
-                product.price = Products[i].price;
-                product.expiration_date = Products[i].expiration_date;
-                product.category = Products[i].category;
-                product.isSwedish = Products[i].isSwedish;
-
-                var json = Newtonsoft.Json.JsonConvert.SerializeObject(product);
-                var data = new System.Net.Http.StringContent(json, Encoding.UTF8, "application/json");
-
-                var url = "http://localhost:5014/Product";
-                using var client = new HttpClient();
-
-                client.DefaultRequestHeaders.ConnectionClose = true;
-
-                var response = await client.PostAsync(url, data);
-
-                string result = response.Content.ReadAsStringAsync().Result;
-                Console.WriteLine(result);
-
-            }
-        }
-*/
-
-
-
-/*
-            System.Net.ServicePointManager.SecurityProtocol =
-            SecurityProtocolType.Tls12 |
-            SecurityProtocolType.Tls11 |
-            SecurityProtocolType.Tls;
-*/
-
-/*
-    using HttpClient client = new ();
-client.DefaultRequestHeaders.Accept.Clear();
-client.DefaultRequestHeaders.Accept.Add(
-    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
-
-
-var repositories = await ProcessRepositoriesAsync(client);
-
-foreach (var repo in repositories)
-{
-    Console.WriteLine($"ID: {repo.Id}");
-    Console.WriteLine($"Name: {repo.Product_title}");
-    Console.WriteLine($"EAN: {repo.Ean_number}");
-    Console.WriteLine($"Description: {repo.Product_description}");
-    Console.WriteLine($"Price: {repo.Price}");
-    Console.WriteLine($"Amount: {repo.Amount_storage}");
-    Console.WriteLine($"Expiration: {repo.Expiration_date}");
-    Console.WriteLine($"Category: {repo.Category}");
-    Console.WriteLine($"Swedish?: {repo.IsSwedish}");
-    Console.WriteLine($"Date: {repo.LastPushUtc}");
-
-    Console.WriteLine();
-}*/
-
-
-
-
-/*
-var pairs = new List<KeyValuePair<string, string>>
-{
-    new KeyValuePair<string, string>("login", "abc")
-};
-
-var content = new FormUrlEncodedContent(pairs);
-
-var client = new HttpClient { BaseAddress = new Uri("http://localhost:7014") };
-
-// call sync
-var response = client.PostAsync("/Product", content).Result;
-if (response.IsSuccessStatusCode)
-{
-}
-*/
-
-/*
-using (var client = new HttpClient())
-{
-    client.BaseAddress = new Uri("http://localhost:7014");
-    var content = new FormUrlEncodedContent(new[]
-    {
-    new KeyValuePair<string, string>("price", "666")
-});
-    var result = await client.PostAsync("/Product", content);
-    string resultContent = await result.Content.ReadAsStringAsync();
-    Console.WriteLine(resultContent);
-}
-*/
-
-
-
-
-//static HttpClient client = new HttpClient();
-//public s
-//tatic string path = "https://localhost:7014/Product";
-
-
-
-/*
-        static async Task OKOO()
-        {
-            HttpClient httpClient = new HttpClient();
-
-            Console.WriteLine("HTTP.. Method");
-
-            /*
-            // Obtain a JWT token.
-            StringContent httpContent = new StringContent(@"{ ""Product_title"": ""Sam"", ""Ean_number"": ""123"" }", Encoding.UTF8, "application/json");
-            var response = await httpClient.PostAsync("https://localhost:44319/api/Authentication/Authenticate", httpContent);
-
-            // Save the token for further requests.
-            var token = await response.Content.ReadAsStringAsync();
-
-            Console.WriteLine(token);
-
-            // Set the authentication header. 
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            */
-
-/*        // Send a request to fetch data.
-          string requestAddress = "https://localhost:7014/Product";
-          var employees = await httpClient.GetAsync(requestAddress);
-          Console.WriteLine("aaaaaaaa" + employees);
-      }
-*/
-
-/*
-
-static async Task<Product> GetProductAsync()
-{
-    Product product = null;
-    HttpResponseMessage response = await client.GetAsync("https://localhost:7014/Product");
-    if (response.IsSuccessStatusCode)
-    {
-        product = await response.Content.ReadAsStringAsync<Product>();
-    }
-    return product;
-}
-*/
